b402e0d67e83b30c08c93a152c39e49ac0d52bd5
Steve Fryatt <info@stevefryatt.org.uk>
2019-02-16 22:35:29 +0000

    Update Licence from Freeware to EUPL.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,29 @@
-/* Launcher - main.c
- * (c) Stephen Fryatt, 2002
+/* Copyright 2002-2019, Stephen Fryatt (info@stevefryatt.org.uk)
  *
- * Desktop application launcher.
+ * This file is part of Launcher:
+ *
+ *   http://www.stevefryatt.org.uk/software/
+ *
+ * Licensed under the EUPL, Version 1.1 only (the "Licence");
+ * You may not use this work except in compliance with the
+ * Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ *
+ *   http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
  */
 
+/**
+ * \file: main.c
+ */
 
 /* ANSI C header files */
 
